92f86bf4c81986caf2502f04c02b43804ed5a343
DorraGara <62667277+DorraGara@users.noreply.github.com>
2024-03-21 17:43:59 +0100

    update port for railway

--- PathFinderApi.py
+++ PathFinderApi.py
@@ -27,4 +27,4 @@ def handle_find_path():
     return jsonify({'path': path})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+        app.run(debug=True, port=os.getenv("PORT", default=5000))
