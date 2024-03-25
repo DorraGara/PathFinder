17d03880ce076572fe267c32d7e6e2c551e7537d
DorraGara <62667277+DorraGara@users.noreply.github.com>
2024-03-25 22:42:14 +0100

    Cors

--- PathFinderApi.py
+++ PathFinderApi.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pathfinder import find_path
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def server_up():
